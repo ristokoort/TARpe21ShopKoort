14672437f68848589bf40c9b40dfc56e2e1ccef7
opilane <opilane@HP-CZC321705R>
2023-11-22 11:35:35 +0200

    Index view

---
+++ TARpe21ShopRisto.Data/Migrations/20231122093140_test.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TARpe21ShopRisto.Data.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Previous_Ownership",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Previous_Ownership",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
